4219e8bb3f18d6248b2ea6903a035933026304dd
jawad <ahmad_abdulgawad.mahmoud@yahoo.com>
2022-02-18 19:59:51 +0300

    Project construction.

---
+++ Scalio/Scalio/ScalioApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScalioApp.swift
+//  Scalio
+//
+//  Created by Ahmed Abduljawad on 18/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct ScalioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
